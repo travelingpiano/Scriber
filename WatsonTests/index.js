b0f971bbe63d36118518ed4eba9b8931fe3f89f7
Spencer Chan <schan1031@gmail.com>
2017-08-05 13:29:53 -0700

    first transcriptions

--- WatsonTests/index.js
+++ WatsonTests/index.js
@@ -24,7 +24,7 @@ var recognizeStream = speech_to_text.createRecognizeStream(params);
 fs.createReadStream('audio-file.flac').pipe(recognizeStream);
 
 // Pipe out the transcription to a file.
-recognizeStream.pipe(fs.createWriteStream('transcription.txt'));
+recognizeStream.pipe(fs.createWriteStream('transcription2.txt'));
 
 // Get strings instead of buffers from 'data' events.
 recognizeStream.setEncoding('utf8');
