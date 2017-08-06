c72e97770d2f2099cef27394db8885c0af070e73
travelingpiano <lihsuanlu@gmail.com>
2017-08-06 13:18:29 -0700

    done basic setup for django auth

---
+++ backend/scriber/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import permissions, routers, viewsets
+from django.contrib.auth.models import User
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
+from scriber.serializers import UserSerializer
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
