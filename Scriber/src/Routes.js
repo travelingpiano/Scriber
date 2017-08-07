f63d044b43e263676cff9fd6e336092bccd994f5
Virginia Chen <virginia.chen32@gmail.com>
2017-08-07 12:11:28 -0700

    Create new splash page

--- Scriber/src/Routes.js
+++ Scriber/src/Routes.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 
-import LoginForm from './components/session/login_form_container';
-import SignupForm from './components/session/signup_form_container';
+import LoginForm from './components/session_form/login_form';
+import SignupForm from './components/session_form/signup_form';
 import Dashboard from './components/dashboard';
 import Splash from './components/splash';
 
 const Routes = () => (
   <Router>
     <Scene
-      key="LoginForm"
-      component={LoginForm}
-      title="Login"
-    />
+      key="Root"
+      title="Root"
+    >
+      <Scene
+        key="LoginForm"
+        component={LoginForm}
+        title="Login"
+      />
 
-    <Scene
-      key="SignupForm"
-      component={SignupForm}
-      title="Signup"
-    />
+      <Scene
+        key="SignupForm"
+        component={SignupForm}
+        title="Signup"
+      />
 
-    <Scene
-      key="Dashboard"
-      component={Dashboard}
-      title="Dashboard"
-    />
+      <Scene
+        key="Dashboard"
+        component={Dashboard}
+        title="Dashboard"
+      />
 
-    <Scene
-      initial
-      key="Splash"
-      component={Splash}
-    />
+      <Scene
+        initial
+        key="Splash"
+        component={Splash}
+      />
+    </Scene>
   </Router>
 
 );
