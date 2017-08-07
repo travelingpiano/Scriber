ffa9ed33745fa0eab3b0726f498ac9c2f8732af4
Spencer Chan <schan1031@gmail.com>
2017-08-07 10:58:51 -0700

    transcription model and join model

--- Scriber/backend/scriber/models.py
+++ Scriber/backend/scriber/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Transcription(models.Model):
+    audio_url = models.CharField()
+    transcription = models.TextField()
+
+class TranscriptionUsers(models.Model):
+    transcription_key = models.ForeignKey(Transcription)
+    user_key = models.ForeignKey(User)
