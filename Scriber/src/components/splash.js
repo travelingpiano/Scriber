13c4cf87c8a93116f82ab95afbea3f6f1af8c5ad
Virginia Chen <virginia.chen32@gmail.com>
2017-08-07 11:59:16 -0700

    Move login code

--- Scriber/src/components/splash.js
+++ Scriber/src/components/splash.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Text,
          StyleSheet,
-         View } from 'react-native';
+         View,
+         Button,
+         Image } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 const Splash = ({ children }) => {
-  const { textStyle, viewStyle } = styles;
+  const { textStyle, viewStyle, buttonStyle } = styles;
 
   return (
     <View style={ viewStyle }>
       <Text style={ textStyle }>
         { children }
       </Text>
+      <View style={ buttonStyle }>
+        <Button
+          onPress={() => Actions.SignupForm()}
+          title="Sign Up"
+        />
+      </View>
+      <View style={ buttonStyle }>
+        <Button
+          onPress={() => Actions.LoginForm()}
+          title="Login"
+        />
+      </View>
     </View>
   );
 };
@@ -18,6 +33,12 @@ const Splash = ({ children }) => {
 export default Splash;
 
 const styles = StyleSheet.create({
+  buttonStyle: {
+    minWidth: 200,
+    margin: 5,
+    backgroundColor: '#FF4242',
+  },
+
   viewStyle: {
 
   },
