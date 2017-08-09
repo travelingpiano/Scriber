62c98662d1eb9b0ef9f2dbe3440694a397846eee
travelingpiano <lihsuanlu@gmail.com>
2017-08-09 11:37:22 -0700

    comment and remove unnecessary and sensitive info

--- Scriber/backend/backend/settings.py
+++ Scriber/backend/backend/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'scriber',
-        'USER': 'lihsuan',
+        # 'USER': 'lihsuan',
         # 'USER': 'virginiachen',
         'PASSWORD': '',
         'HOST': 'localhost',
