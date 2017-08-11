2d2eada2bd70766835f5b8db4097788b36b7ecf6
Virginia Chen <virginia.chen32@gmail.com>
2017-08-10 21:24:10 -0700

    Speaker categories and timestamps

--- Scriber/src/components/transcription/full_transcription.js
+++ Scriber/src/components/transcription/full_transcription.js
@@ -14,15 +14,39 @@ class FullTranscription extends Component {
     );
   }
 
+  // NOTE: not finished/needed? Delete this if not going to use!
   // returns the time of the snippet as an absolute time (createdTime plus timestamp start time)
   // createdTime format: 'XX:XX:XX.XXXX'
   // time format: X.X.XX
   absoluteTime(createdTime, time) {
+    let parsed = '';
+    let createdTimeInt = createdTime.split(':');
+    let createdHour = parseInt(createdTimeInt[0]);
+    let createdMin = parseInt(createdTimeInt[1]);
+    let createdSec = parseInt(createdTimeInt[2].substring(0,2));
+    console.log(createdHour,createdMin,createdSec);
+    let currentTime = time.toString().split('.');
+    let currentHour = 0;
+    let currentMin = 0;
+    if (currentTime.length === 3) {
+      currentHour = currentTime[0];
+    }
+  }
 
+  renderSpeaker(snippet) {
+    if (JSON.parse(snippet).speaker === this.currentSpeaker) {
+      return null;
+    } else {
+      this.currentSpeaker = JSON.parse(snippet).speaker;
+      return (
+        <Text style={styles.speaker}>Speaker: {JSON.parse(snippet).speaker}</Text>
+      );
+    }
   }
 
   render() {
     let allSnippets = null;
+    this.currentSpeaker = '';
     if (this.props.transcription) {
       let { transcription, createdTime } = this.props;
       console.log(this.props);
@@ -32,9 +56,9 @@ class FullTranscription extends Component {
         console.log(JSON.parse(snippet).timestamps[0]);
         return (
           <View key={`snippet-${idx}`}>
-            <Text>Speaker: {JSON.parse(snippet).speaker}</Text>
+            {this.renderSpeaker(snippet)}
+            <Text style={styles.timeStamps}>{JSON.parse(snippet).timestamps[0]}</Text>
             <Text>{JSON.parse(snippet).text}</Text>
-            <Text>{JSON.parse(snippet).timestamps[0]}</Text>
           </View>
         );
       });
@@ -61,6 +85,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 10
   },
+
+  speaker: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingTop: 15,
+  },
+
+  timeStamps: {
+    textAlign: 'right',
+    paddingTop: 10,
+  }
 
 
 });
