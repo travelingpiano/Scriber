34e2afa9908c5079949e3a74a2b884e9843923c6
Virginia Chen <virginia.chen32@gmail.com>
2017-08-10 19:45:12 -0700

    Format time

--- Scriber/src/components/transcription/transcription_show_container.js
+++ Scriber/src/components/transcription/transcription_show_container.js
@@ -7,8 +7,6 @@ import TranscriptionShow from './transcription_show';
 // import {clearErrors} from '../../actions/error_actions';
 
 const mapStateToProps = (state,ownProps) => {
-  console.log(state.transcriptions);
-  console.log(ownProps);
   return {
     currentTranscription: state.transcriptions.currentTranscription
     // currentUser: state.session.currentUser,
