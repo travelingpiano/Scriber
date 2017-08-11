47078283481e4008a4d9a8ea29aeb5bea7b458b4
Ade Farquhar <adefarquhar@gmail.com>
2017-08-11 10:21:45 -0700

    edit user reducer

--- Scriber/src/reducers/users_reducer.js
+++ Scriber/src/reducers/users_reducer.js
@@ -8,7 +8,7 @@ const UsersReducer = (state = defaultState(), action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_USERS:
-      return Object.assign({}, )
+      return Object.assign({}, state, action.users);
     default:
       return state;
   }
