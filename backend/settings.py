60070386001b0feabdcbf4e97ea650f6c2f13ba9
Spencer Chan <schan1031@gmail.com>
2017-08-12 11:18:43 -0700

    heroku postgres setup

--- backend/settings.py
+++ backend/settings.py
@@ -106,18 +106,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'scriber',
-        # 'USER': 'lihsuan',
-        # 'USER': 'virginiachen',
-        # 'USER': 'adefarquhar',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    'default': dj_database_url.config(default=os.environ["DATABASE_URL"])
 }
+
+# DATABASES = {
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'scriber',
+#         'USER': 'lihsuan',
+#         'USER': 'virginiachen',
+#         'USER': 'adefarquhar',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
