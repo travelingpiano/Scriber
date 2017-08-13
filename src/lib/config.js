ce0837fd2eb2ec6f527789d3c580ea31a8e515f8
Virginia Chen <virginia.chen32@gmail.com>
2017-08-12 22:32:59 -0700

    Ready to pull request

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,8 +4,8 @@ module.exports = {
   // client_id: 'h2d9jm4dI7EGJq2yC5Tm3lvJHqp9qxbWN8SDhHhD',
 
   // virginia
-  // client_key: 'eOw9DVNKzMmRkvqrwSpqEAV74ERQ60H51yoj7BjNV3kXytKqRwwSNQyAC0PSYNaLjkwVgSU6zPtdFnNNRjbnKWJhKoAf6fxT250O6f3DlRAfeaK4bx7KWIVLt9FKol90',
-  // client_id: 'LkqS9l22dpTDuVDB1u540vDJLeCwP0gMEEBK32Dv',
+  // client_key: 'DmvcsEplLS7LgSHHe9teq0iC6346sWTNeY17MIUO6lmBgj7aGuTlr9CRB3B93EDbdmnfyrVot8DQz8IEqm2nSoNnQK60jUgvrrBNKh1abmjzJSxUIkQ3dold8UQQnYh8',
+  // client_id: 'KtDnxIqm4QISxCqtwCMY163npq6maSzBqsjd5ecy',
 
   // ade
   // client_key: 'qDX7tj0nj7Q56qZGPYQXHy6G12KXjQq0a6D9PVRFk4u9bZtNzrBRO3ETP3X02mKS3ZuTgebiuvfrXl0f8sCawc280btM19vzShBDUrroICDoSSdgoU57JAXj7Q94D0zN',
