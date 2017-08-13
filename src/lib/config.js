78cb3683c550487b956d3ab51114afeaf68c95c4
Ade Farquhar <adefarquhar@gmail.com>
2017-08-12 21:00:56 -0700

    prep for merge

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // li hsuan
-  client_key: 'uxzVtXXFnh0LneTLzZbnZ58ZwrvtQmFGhS550j6igISuTRgOseC2zEKwLs4zxLnz5qtfFH4JbGrtMdeTZVIKkxdWp6jbotl9KSqrNPGbIjQLk3oYEQTyI1fmYfzgusK3',
-  client_id: 'h2d9jm4dI7EGJq2yC5Tm3lvJHqp9qxbWN8SDhHhD',
+  // client_key: 'uxzVtXXFnh0LneTLzZbnZ58ZwrvtQmFGhS550j6igISuTRgOseC2zEKwLs4zxLnz5qtfFH4JbGrtMdeTZVIKkxdWp6jbotl9KSqrNPGbIjQLk3oYEQTyI1fmYfzgusK3',
+  // client_id: 'h2d9jm4dI7EGJq2yC5Tm3lvJHqp9qxbWN8SDhHhD',
 
   // virginia
   // client_key: 'eOw9DVNKzMmRkvqrwSpqEAV74ERQ60H51yoj7BjNV3kXytKqRwwSNQyAC0PSYNaLjkwVgSU6zPtdFnNNRjbnKWJhKoAf6fxT250O6f3DlRAfeaK4bx7KWIVLt9FKol90',
   // client_id: 'LkqS9l22dpTDuVDB1u540vDJLeCwP0gMEEBK32Dv',
 
   // ade
-  client_key: 'qDX7tj0nj7Q56qZGPYQXHy6G12KXjQq0a6D9PVRFk4u9bZtNzrBRO3ETP3X02mKS3ZuTgebiuvfrXl0f8sCawc280btM19vzShBDUrroICDoSSdgoU57JAXj7Q94D0zN',
-  client_id: 'p5kvXkN0IzAVoUzdqvLtbEHcx8D3pjhAIjRKnb0n',
+  // client_key: 'qDX7tj0nj7Q56qZGPYQXHy6G12KXjQq0a6D9PVRFk4u9bZtNzrBRO3ETP3X02mKS3ZuTgebiuvfrXl0f8sCawc280btM19vzShBDUrroICDoSSdgoU57JAXj7Q94D0zN',
+  // client_id: 'p5kvXkN0IzAVoUzdqvLtbEHcx8D3pjhAIjRKnb0n',
 };
